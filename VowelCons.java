9123100e224199ac3e862e14aa28902d621f5503
jeevan <jeevanjnayak@gmail.com>
2022-01-24 16:08:36 +0530

    Determining if a letter is vowel or consonant

---
+++ VowelCons.java
@@ -0,0 +1,19 @@
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class CondMonthWage {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter a letter : ");
+        Scanner sc = new Scanner(System.in);
+        char Letter = sc.next().charAt(0);
+
+        if(Letter  == 'a' || Letter  == 'e' || Letter  == 'i' || Letter  == 'o' || Letter  == 'u')
+            System.out.println(Letter+" is vowel");
+
+        else
+            System.out.println(Letter+" is Consonant");
+    }
+}
