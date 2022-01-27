f870a8fc39b38077f417c9da7fff333d191fccca
jeevan <jeevanjnayak@gmail.com>
2022-01-27 16:31:03 +0530

    printing a 2d array using PrintWritter with user input

--- CommandDist.java
+++ CommandDist.java
@@ -1,16 +1,45 @@
 package com.Bridgelabz;
 
+import java.util.Scanner;
+import java.io.PrintWriter;
 
-import static java.lang.Math.sqrt;
+public class TwoDArray {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the Array length :  ");
+        Scanner sc = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+        int n = sc.nextInt();
+        s
+        int[] arr =new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int numSet = 0;
+        for (int i=0; i<n-2; i++)
+        {
+            for (int j=i+1; j<n-1; j++)
+            {
+                for (int k=j+1; k<n; k++)
+                {
+                    if (arr[i]+arr[j]+arr[k] == 0)
+                    {
+                        System.out.print(arr[i]);
+                        System.out.print(" ");
+                        System.out.print(arr[j]);
+                        System.out.print(" ");
+                        System.out.print(arr[k]);
+                        System.out.print("\n");
+                        numSet++;
+                    }
+                }
+            }
+        }
+
+        // If no triplet with 0 sum found in array
+        if (numSet == 0)
+            System.out.println(" not exist ");
 
 
-class CommandDist {
-
-    public static void main(String[] args){
-        int Dist;
-        int x = Integer.parseInt(args[0]);
-        int y = Integer.parseInt(args[1]);
-        Dist = (int) sqrt(x * x + y * y);
-        System.out.println("distance is: " +Dist);
     }
 }
